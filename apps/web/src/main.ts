812388312c5f778919521f042ecda4ccffe0aeee
Dmitry Orlov <d.orlov777@gmail.com>
2026-04-06 18:27:31 +0300

    feat: create app structure

---
+++ apps/web/src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/lara-light-blue/theme.css'
+import 'primeicons/primeicons.css'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(PrimeVue)
+app.mount('#app')
